ed0bfbbef09f9186310f4fe1b128e7bc73216cdf
hwicode <khc3796@naver.com>
2024-11-17 12:49:31 +0900

    [v1] 동시에 100명이 재고 감소를 요청하면 동시성 문제가 발생

--- src/test/java/com/example/managestockconcurrency/StockServiceTest.java
+++ src/test/java/com/example/managestockconcurrency/StockServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
@@ -33,5 +37,27 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+    @Test
+    void 동시에_100명의_유저가_재고_감소_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isZero();
     }
 }
